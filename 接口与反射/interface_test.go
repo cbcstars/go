a128729e7fdd9a33341b53fa5c1ffcbdf3afee05
baicun.chen <baicun.chen@qingteng.cn>
2024-03-05 14:39:42 +0800

    interface

--- 接口与反射/interface_test.go
+++ 接口与反射/interface_test.go
@@ -53,6 +53,30 @@ if v, ok := varI.(T); ok {  // checked type assertion
 2:可以用 type-switch 进行运行时类型分析，但是在 type-switch 不允许有 fallthrough
 */
 
+// 使用方法集与接口(Demo6)
+/*
+1:在接口上调用方法时，必须有和方法定义时相同的接收者类型或者是可以根据具体类型 P 直接辨识的：
+	a:指针方法可以通过指针调用
+	b:值方法可以通过值调用
+	c:接收者是值的方法可以通过指针调用，因为指针会首先被解引用
+	d:接收者是指针的方法不可以通过值调用，因为存储在接口中的值没有地址
+将一个值赋值给一个接口时，编译器会确保所有可能的接口方法都可以在此值上被调用，因此不正确的赋值在编译期就会失败。
+
+2:Go 语言规范定义了接口方法集的调用规则：
+	a:类型 *T 的可调用方法集包含接受者为 *T 或 T 的所有方法集
+	b:类型 T 的可调用方法集包含接受者为 T 的所有方法
+	c:类型 T 的可调用方法集不包含接受者为 *T 的方法
+*/
+
+// 空接口(Demo7)
+/*
+1:空接口或者最小接口 不包含任何方法，它对实现不做任何要求：
+	type Any interface {}
+2:可以给一个空接口类型的变量 var val interface {} 赋任何类型的值。
+*/
+
+// 实战:使用Sorted排序(Demo8)
+
 // Demo1
 // Shaper 图形
 type Shaper interface {
@@ -188,4 +212,136 @@ func TestShaper4(t *testing.T) {
 	if v, ok := shaper.(*Rectangle); ok {
 		fmt.Println(v.Area())
 	}
+}
+
+// Demo5:类型判断
+func TestTypeSwitch(t *testing.T) {
+	var shaper Shaper
+	//var shaper Shaper = &Square{Side: 5}
+
+	switch t := shaper.(type) {
+	case *Square:
+		fmt.Printf("type square %T with value %v \n", t, t)
+	case *Rectangle:
+		fmt.Printf("type rectangle %T with value %v \n", t, t)
+	case nil:
+		fmt.Printf("nil value: nothing to check \n")
+	default:
+		fmt.Printf("unexpected type %T \n", t)
+	}
+
+	switch shaper.(type) {
+	case *Square:
+		// pass
+	case *Rectangle:
+		// pass
+	default:
+		// pass
+	}
+}
+
+// Demo6:接口方法集的调用规则
+type Appender interface {
+	Append(int)
+}
+
+func CountInto(a Appender, start, end int) {
+	for i := start; i < end; i++ {
+		a.Append(i)
+	}
+}
+
+type Lener interface {
+	Len() int
+}
+
+func LongEnough(l Lener) bool {
+	return l.Len()*10 > 42
+}
+
+type List []int
+
+func (listPtr *List) Append(i int) {
+	*listPtr = append(*listPtr, i)
+}
+
+func (listVal List) Len() int {
+	return len(listVal)
+}
+
+func TestList(t *testing.T) {
+	listVal := List{1, 2}
+	listVal.Append(3)
+	fmt.Println(listVal.Len())
+	CountInto(&listVal, 5, 10)
+	LongEnough(listVal)
+	fmt.Println(listVal)
+
+	listPtr := &List{1, 2}
+	listPtr.Append(3)
+	fmt.Println(listPtr.Len())
+	CountInto(listPtr, 5, 10)
+	LongEnough(listPtr)
+	fmt.Println(listPtr)
+}
+
+// Demo7:空接口
+type Any interface {
+}
+
+func TestAny(t *testing.T) {
+	var a Any
+	a = 1
+	a = "string"
+	a = struct {
+		Name string
+		Age  int
+	}{"小陈", 18}
+	fmt.Println(a)
+}
+
+func TestAny2(t *testing.T) {
+	var a Any = "1234"
+	if v, ok := a.(int); ok {
+		fmt.Println(v)
+	}
+	fmt.Println("测试")
+}
+
+// Demo8:使用Sorter接口排序
+type Sorter interface {
+	Len() int
+	Less(i, j int) bool
+	Swap(i, j int)
+}
+
+// 冒泡排序
+func Sort(data Sorter) {
+	for pass := 1; pass < data.Len(); pass++ {
+		for i := 0; i < data.Len()-pass; i++ {
+			if data.Less(i, i+1) {
+				data.Swap(i, i+1)
+			}
+		}
+	}
+}
+
+type IntArray []int
+
+func (a IntArray) Len() int {
+	return len(a)
+}
+
+func (a IntArray) Less(i, j int) bool {
+	return a[i] > a[j]
+}
+
+func (a IntArray) Swap(i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
+
+func TestIntArray(t *testing.T) {
+	intSlice := IntArray{3, 5, 6, 2, 1}
+	Sort(intSlice)
+	fmt.Println(intSlice)
 }
